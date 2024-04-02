b90633726084ae051e974aea16b9d82d841036b9
Ikrar Akmal Ismudibyo <ikrar15akmal@gmail.com>
2024-04-02 21:14:37 +0700

    UI about

--- app/src/main/java/org/d3if0093/assessment/navigation/Screen.kt
+++ app/src/main/java/org/d3if0093/assessment/navigation/Screen.kt
@@ -3,5 +3,6 @@ package org.d3if0093.assessment.navigation
 import org.d3if0093.assessment.ui.screen.MainScreen
 
 sealed class Screen(val route: String){
-    data object Home: Screen("MainScreen")
+    data object Home: Screen("mainScreen")
+    data object About: Screen("aboutScreen")
 }
